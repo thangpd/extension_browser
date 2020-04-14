99f141052bc6c0c83f09c8050f661bb824d9cbc4
thang <thangpd.slz@gmail.com>
2020-04-14 13:43:10 +0700

    update extension login api

--- assets/js/content.js
+++ assets/js/content.js
@@ -26,23 +26,53 @@ function getCookie(cname) {
     return "";
 }
 
-if (chrome.extension != undefined)
-    var src_image = chrome.extension.getURL("assets/image/icon.png");
+
+function setStorage(obj) {
+    if (typeof (obj) === Object) {
+        chrome.storage.sync.set(obj, function () {
+            console.log('Value is set to ' + value);
+        });
+    } else {
+        console.log('Set storage, input param must be object');
+    }
+}
+
+function getStorage(key) {
+    console.log(key);
+    chrome.storage.sync.get(null, function (result) {
+        console.log(result);
+    });
+}
+
+
+var src_image = chrome.extension.getURL("assets/image/icon48.png");
 $(document).ready(function () {
-    var format_html = '<div id="extension"><div class="ext-banner ext-banner-2" style="background-image: url(\'' + src_image + '\');"> <div class="content"><a href="https://hoantienmuasam.com/" class="action-btn">Kích hoạt hoàn tiền</a>  <a class="ignore-btn">Kích hoạt sau</a>  </div></div> </div>';
-    var format_html_exists = '<div id="extension"><div class="ext-banner ext-banner-2" style="background-image: url(\'' + src_image + '\');"> <div class="content"><a href="#" class="action-btn active">Đã kích hoạt</a>  </div></div> </div>';
+    var params = '?site=' + window.location.host;
+    var url_redirect = 'http://localhost/hoantien/auth-page'
+    var format_html = '<div id="extension"><div class="ext-banner ext-banner-2" style="background-image: url(\'' + src_image + '\');"> <div class="content"><a href="' + url_redirect + '" class="hoantien_action-btn">Kích hoạt hoàn tiền</a>  <a class="ignore-btn">Kích hoạt sau</a>  </div></div> </div>';
+    var format_html_exists = '<div id="extension"><div class="ext-banner ext-banner-2" style="background-image: url(\'' + src_image + '\');"> <div class="content"><a href="#" class="hoantien_action-btn active">Đã kích hoạt</a>  </div></div> </div>';
 
 
-    if (chrome != undefined)
-        chrome.runtime.onMessage.addListener(gotMessage);
+    /*var format_login_signup = '<div class="login-box"> <div class="lb-header"><a href="#" class="active" id="login-box-link">Login</a> <a href="#" id="signup-box-link">Sign Up</a> <button class="login-box-close">X</button> </div> <div class="social-login"><a href="#"> <i class="fa fa-facebook fa-lg"></i> Login in with facebook </a> <a href="#"> <i class="fa fa-google-plus fa-lg"></i> log in with Google </a></div> <form class="email-login"> <div class="u-form-group"><input type="text" class="hoantien_user" placeholder="User"/></div> <div class="u-form-group"><input type="password" class="hoantien_password" placeholder="Password"/></div> <div class="u-form-group"> <button class="hoantien_login">Log in</button> </div> </form> <form class="email-signup"> <div class="u-form-group"><input type="text" class="hoantien_display" placeholder="Display Name"/></div> <div class="u-form-group"><input type="text" class="hoantien_user" placeholder="User"/></div> <div class="u-form-group"><input type="password" class="hoantien_password" placeholder="Password"/></div> <div class="u-form-group"> <button class="hoantien_signup">Sign Up</button> </div> </form> </div>';
+
+
+    $('body').append(format_login_signup);
+    $(".email-signup").hide();
+    $("#signup-box-link").click(function () {
+        $(".email-login").fadeOut(100);
+        $(".email-signup").delay(100).fadeIn(100);
+        $("#login-box-link").removeClass("active");
+        $("#signup-box-link").addClass("active");
+    });
+    $("#login-box-link").click(function () {
+        $(".email-login").delay(100).fadeIn(100);
+        $(".email-signup").fadeOut(100);
+        $("#login-box-link").addClass("active");
+        $("#signup-box-link").removeClass("active");
+    });*/
+    chrome.runtime.onMessage.addListener(gotMessage);
 
     function gotMessage(res, sender, sendResponse) {
-        // let cookie = getCookie("testtest");
-        // if (cookie == "") {
-        //     setCookie("testtest", "testyesah", 1);
-        // } else {
-        // }
-        console.log('okj');
         toogle_popup();
     }
 
@@ -67,7 +97,7 @@ $(document).ready(function () {
     }
 
     function success_active() {
-        let action_btn = $(document).find('#extension .action-btn');
+        let action_btn = $(document).find('#extension .hoantien_action-btn');
         console.log(action_btn);
         action_btn.addClass('active');
         action_btn.html('Đã Kích Hoạt');
@@ -105,7 +135,6 @@ $(document).ready(function () {
         switch (window.location.host) {
             case 'tiki.vn':
                 let cookie_tiki = getCookie("TIKI_B2B_AFFILIATE_INFO");
-                console.log('ok');
                 console.log(cookie_tiki)
                 if (cookie_tiki !== "") {
                     success_active();
@@ -125,7 +154,7 @@ $(document).ready(function () {
         setCookie("temp_hide_extension", "yes", 1);
     }
 
-    $(document).on('click', '.action-btn', function (e) {
+    $(document).on('click', '.hoantien_action-btn', function (e) {
         auto_hide_when_exists();
     }).on('click', '.ignore-btn', function (e) {
         set_cookie_temp_hide();
@@ -134,22 +163,78 @@ $(document).ready(function () {
 
     show_popup();
     auto_hide_when_exists();
-    $('body').append(format_login_form);
-})
 
-$(".email-signup").hide();
-$("#signup-box-link").click(function(){
-    $(".email-login").fadeOut(100);
-    $(".email-signup").delay(100).fadeIn(100);
-    $("#login-box-link").removeClass("active");
-    $("#signup-box-link").addClass("active");
+    /*
+        $('button.hoantien_login').on('click', function (e) {
+            e.preventDefault();
+            console.log('login');
+            let user = $(document).find('.email-login .hoantien_user').val();
+            console.log(user);
+
+            if (user && false) {
+                var res = getNonceUser();
+                console.log(res);
+                if (res.nonce) {
+                    var cookie_generated = generateCookie(res.nonce);
+                    console.log(cookie_generated);
+                } else {
+                    console.log('missing nonce');
+                }
+            }
+        })
+        $('button.hoantien_signup').on('click', function (e) {
+            e.preventDefault();
+            console.log('signup');
+            var url = '';
+            $.ajax({
+                method: "POST",
+                url: url,
+            }).done(function (res) {
+                console.log(res);
+            })
+
+        });
+
+        function validateCookieUser() {
+
+        }
+
+
+        function generateCookie(nonce, user, pass) {
+            if (nonce && user && pass) {
+                var url = 'https://hoantienmuasam.com/api/user/generate_auth_cookie/?nonce=' + nonce + '&username=' + user + '&password=' + pass;
+                var res = $.ajax({
+                    method: "POST",
+                    url: url,
+                    async: false,
+                }).done(function (res) {
+                    console.log(res);
+                    return res;
+                })
+                return res.responseJSON;
+            } else {
+                console.log('Missing Nonce or user or pass');
+            }
+        }
+
+
+        function getNonceUser() {
+            var url = 'https://hoantienmuasam.com/api/get_nonce/?controller=user&method=generate_auth_cookie';
+            var res = $.ajax({
+                method: "POST",
+                url: url,
+                async: false,
+            }).done(function (res) {
+                return res;
+            })
+            return res.responseJSON;
+        }*/
 });
-$("#login-box-link").click(function(){
-    $(".email-login").delay(100).fadeIn(100);;
-    $(".email-signup").fadeOut(100);
-    $("#login-box-link").addClass("active");
-    $("#signup-box-link").removeClass("active");
-});
+
+
+
+
+
 
 
 
--- index.php
+++ index.php
@@ -14,60 +14,14 @@
 </head>
 <body>
 <button class="btn btn-success">test</button>
-
-<div class="login-box">
-    <div class="lb-header">
-        <a href="#" class="active" id="login-box-link">Login</a>
-        <a href="#" id="signup-box-link">Sign Up</a>
-    </div>
-    <div class="social-login">
-        <a href="#">
-            <i class="fa fa-facebook fa-lg"></i>
-            Login in with facebook
-        </a>
-        <a href="#">
-            <i class="fa fa-google-plus fa-lg"></i>
-            log in with Google
-        </a>
-    </div>
-    <form class="email-login">
-        <div class="u-form-group">
-            <input type="email" placeholder="Email"/>
-        </div>
-        <div class="u-form-group">
-            <input type="password" placeholder="Password"/>
-        </div>
-        <div class="u-form-group">
-            <button>Log in</button>
-        </div>
-        <div class="u-form-group">
-            <a href="#" class="forgot-password">Forgot password?</a>
-        </div>
-    </form>
-    <form class="email-signup">
-        <div class="u-form-group">
-            <input type="email" placeholder="Email"/>
-        </div>
-        <div class="u-form-group">
-            <input type="password" placeholder="Password"/>
-        </div>
-        <div class="u-form-group">
-            <input type="password" placeholder="Confirm Password"/>
-        </div>
-        <div class="u-form-group">
-            <button>Sign Up</button>
-        </div>
-    </form>
-</div>
+<div class="login-box"> <div class="lb-header"><a href="#" class="active" id="login-box-link">Login</a> <a href="#" id="signup-box-link">Sign Up</a> <button class="login-box-close">X</button> </div> <div class="social-login"><a href="#"> <i class="fa fa-facebook fa-lg"></i> Login in with facebook </a> <a href="#"> <i class="fa fa-google-plus fa-lg"></i> log in with Google </a></div> <form class="email-login"> <div class="u-form-group"><input type="text" class="hoantien_user" placeholder="User"/></div> <div class="u-form-group"><input type="password" class="hoantien_password" placeholder="Password"/></div> <div class="u-form-group"> <button class="hoantien_login">Log in</button> </div> </form> <form class="email-signup"> <div class="u-form-group"><input type="text" class="hoantien_display" placeholder="Display Name"/></div> <div class="u-form-group"><input type="text" class="hoantien_user" placeholder="User"/></div> <div class="u-form-group"><input type="password" class="hoantien_password" placeholder="Password"/></div> <div class="u-form-group"> <button class="hoantien_signup">Sign Up</button> </div> </form> </div>
 
 <script src="assets/js/content.js"></script>
 </body>
 </html>
 
-<?php
-/**
- * Created by PhpStorm.
- * User: abc
- * Date: 4/13/2020
- * Time: 1:14 AM
- */
+
+
+
+
+
